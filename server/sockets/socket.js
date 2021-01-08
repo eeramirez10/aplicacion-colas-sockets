ec67449bb03835f67f80ef123de67f5ceb17b438
eeramirez10 <eeramirez@tuvansa.com.mx>
2021-01-08 13:11:25 -0600

    Aplicacion de colas con sockets

---
+++ server/sockets/socket.js
@@ -0,0 +1,59 @@
+const { io } = require('../server');
+
+const { TicketControl } = require('../classes/ticket-control');
+
+const ticketControl = new TicketControl();
+
+
+
+io.on('connection', (client) => {
+
+    //console.log('Usuario conectado');
+
+    client.on('siguienteTicket', () => {
+
+        let siguiente = ticketControl.siguiente();
+
+        client.emit('siguienteTicket', siguiente);
+    })
+
+
+
+
+
+    client.emit('estadoActual', {
+        actual: ticketControl.getUltimoTicket(),
+        ultimos4: ticketControl.getUltimos4()
+    });
+
+    /*     client.emit('ultimos4',{
+            ultimos4: ticketControl.getUltimos4()
+        }) */
+
+ 
+
+    client.on('atenderTicket', (data, callback) => {
+        if (!data.escritorio) {
+            return callback({
+                err: true,
+                mensaje: 'El escritorio es necesario'
+            });
+        }
+
+        let atenderTicket = ticketControl.atenderTicket(data.escritorio);
+
+        callback(atenderTicket);
+
+        client.broadcast.emit('ultimos4', {
+            ultimos4: ticketControl.getUltimos4()
+        })
+
+
+     
+
+
+
+    })
+
+
+});
